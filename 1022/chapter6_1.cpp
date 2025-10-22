ed370a275a36495ece790803d29620a587726fe5
Siyun.Gwak <geunji04@g.eulji.ac.kr>
2025-10-22 16:03:51 +0900

    1022

---
+++ 1022/chapter6_1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+//두 수 중 큰 값을 반환
+int max(int num1, int num2)
+{
+    int result;
+
+    if (num1 > num2)
+    result = num1;
+    else
+    result = num2;
+
+    return result;
+}
+
+int main()
+{
+    int i = 5;
+    int j = 2;
+    int k = max(i,j);
+    cout <<"The maximum between " << i << " and " << j << " is " << k << endl;
+
+    return 0;
+}
